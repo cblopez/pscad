da17a80c95f228135fc64c828545d7ba54067c9e
Christian Barral López <christianbarrallopez@Christians-MacBook-Pro.local>
2019-06-23 02:29:21 +0200

    Cross platform UNIX installer

--- install.sh
+++ install.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-apt-get install nmap
+APT_GET_CMD=$(which apt-get)
+APTITUDE_CMD=$(which aptitude)
+YUM_CMD=$(which yum)
+DNF_YUM_CMD=$(which dnf)
+PACMAN_CMD=$(which pacman)
+BREW_CMD=$(which brew)
+
+
+if [[ ! -z $APT_GET_CMD ]]; then
+	apt-get install nmap pip
+elif [[ ! -z $APTITUDE_CMD ]]; then
+	aptitude install nmap pip
+elif [[ ! -z $YUM_CMD ]]; then
+	yum install nmap pip
+elif [[ ! -z $DNF_YUM_CMD ]]; then
+	dnf install nmap pip
+elif [[ ! -z $PACMAN_CMD ]]; then
+	pacman -S nmap pip
+elif [[ ! -z $BREW_CMD ]]; then
+	brew install nmap pip
+else
+	echo "Unhandled Package Manager. Please install manually."
+	exit 1;
+fi
+
 
 pip install --upgrade pip
 
